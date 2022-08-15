3dbc6355a4060112acdabc3d918380cb7112a1f7
Lavanya <lavenyahs@gmail.com>
2022-08-15 01:30:26 -0400

    try

--- HRMapp/src/main/java/com/hrmapp/base/BaseClass.java
+++ HRMapp/src/main/java/com/hrmapp/base/BaseClass.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
